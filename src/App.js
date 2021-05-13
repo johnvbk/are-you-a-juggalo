9dd7cf0089a666bc7cb4d1d1dcbcba21d22cdc33
johnvbk <31219912+johnvbk@users.noreply.github.com>
2021-05-12 18:15:32 -0700

    Update App.js
    typo

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
           {id: 1, question: "Do you drink Faygo", possible_answers: ['Yes', 'Maybe', 'No'], answer: null, juggalo_answer: 'Yes', available: true, image: '/images/juggalette-faygo.png'},
           {id: 2, question: "Is Eminem a little bitch", possible_answers: ['Fo Sho', 'No'], answer: null, juggalo_answer: 'Fo Sho', available: false, image: '/images/eminem_bitch.png'},
           {id: 3, question: "Did Chuck cut 'em up", possible_answers: ['Yes', 'No'], answer: null, juggalo_answer: 'Yes', available: false, image: '/images/chuck_chainsaw.png'},
-          {id: 4, question: "Were you jumped you in" , possible_answers: ['Yes', 'No'], answer: null, juggalo_answer: 'Yes', available: false, image: '/images/juggalo_gang.png'},
+          {id: 4, question: "Were you jumped in" , possible_answers: ['Yes', 'No'], answer: null, juggalo_answer: 'Yes', available: false, image: '/images/juggalo_gang.png'},
           {id: 5, question: "Do you know how magnets work" , possible_answers: ['Yes', 'No'], answer: null, juggalo_answer: 'No', available: false, image: '/images/magnets.png'},
           {id: 6, question: "How do scientist make you feel", possible_answers: ['Pissed', 'Informed'],  answer: null, juggalo_answer: 'Pissed', available: false, image: '/images/fuck_science.png'}
         ],
